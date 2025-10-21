cceb13b665ca9ca48270b88f93bbe6e2331d3c3f
Matt <mattsince87@gmail.com>
2025-10-21 16:51:49 +0300

    Fixed project create modal

--- apps/dashboard/src/app/(dashboard)/[organizationId]/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/[organizationId]/layout.tsx
@@ -1,7 +1,14 @@
 export default async function Layout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
-  return <div className="w-full flex-1">{children}</div>;
+  return (
+    <div className="w-full flex-1">
+      {children}
+      {modal}
+    </div>
+  );
 }
