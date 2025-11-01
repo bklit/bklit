78b3d5230cc3fc7d37bf195a8753555610687cbf
Matt <mattsince87@gmail.com>
2025-11-01 18:55:02 +0200

    matt/bkl-9-lightmode

--- apps/dashboard/src/app/(dashboard)/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/layout.tsx
@@ -17,7 +17,7 @@ export default async function DashboardLayout({
     <WorkspaceProvider session={session} organizations={organizations}>
       <div className="flex flex-col min-h-screen ">
         <SiteHeader />
-        <main className="flex-1 flex flex-col bg-black">{children}</main>
+        <main className="flex-1 flex flex-col bg-background">{children}</main>
         {modal}
       </div>
     </WorkspaceProvider>
