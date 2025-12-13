88804703dd45dee9e0d77d004289081a179ca20c
Matt <mattsince87@gmail.com>
2025-12-14 01:13:58 +0200

    Docs update

--- apps/docs/source.config.ts
+++ apps/docs/source.config.ts
@@ -2,12 +2,15 @@ import { defineConfig, defineDocs } from "fumadocs-mdx/config";
 
 export const docs = defineDocs({
   dir: "content/docs",
-  code: {
-    themes: {
-      light: "vitesse-light",
-      dark: "vitesse-black",
+});
+
+export default defineConfig({
+  mdxOptions: {
+    rehypeCodeOptions: {
+      themes: {
+        light: "vitesse-light",
+        dark: "vitesse-black",
+      },
     },
   },
 });
-
-export default defineConfig();
