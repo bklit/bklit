f3cd000f47654678884abdd071fe1c3513699c8a
Matt <mattsince87@gmail.com>
2025-12-13 22:03:16 +0200

    Updated website with new doc links

--- apps/website/src/components/footer.tsx
+++ apps/website/src/components/footer.tsx
@@ -65,6 +65,18 @@ export const Footer = () => {
                   </Link>
                 </li>
                 <li>
+                  <a
+                    href="https://docs.bklit.com"
+                    className="flex items-center gap-2 group transition-all duration-200 relative hover:text-foreground opacity-50"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="text-lime-200 transition absolute left-0 opacity-0 group-hover:opacity-100">
+                      ◑
+                    </span>
+                  </a>
+                </li>
+                <li>
                   <Link
                     href="/"
                     className="flex items-center gap-2 group transition-all duration-200 relative hover:text-foreground opacity-50"
