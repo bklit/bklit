8ceba5dfe36ef8d226a281e6ccfa7a8d58063d2c
Matt <mattsince87@gmail.com>
2025-12-13 17:43:13 +0200

    various mobile tweaks

--- apps/dashboard/src/app/(dashboard)/layout.tsx
+++ apps/dashboard/src/app/(dashboard)/layout.tsx
@@ -23,7 +23,7 @@ export default async function DashboardLayout({
           <AppSidebar />
 
           <SidebarInset>
-            <main className="flex flex-col dark:bg-bklit-800 bg-zinc-50 border border-border rounded-xl overflow-auto h-0 min-h-full p-8 relative">
+            <main className="flex flex-col bg-background dark:sm:bg-bklit-800 sm:bg-zinc-50 sm:border border-border rounded-xl sm:overflow-auto h-0 min-h-full relative sm:p-8">
               {children}
             </main>
             {modal}
