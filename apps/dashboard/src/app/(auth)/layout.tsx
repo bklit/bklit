b6d51f3d90683606a66e541815f4fa029310bdc6
Matt <mattsince87@gmail.com>
2025-12-13 11:26:15 +0200

    Refined auth flow pages (#116)

--- apps/dashboard/src/app/(auth)/layout.tsx
+++ apps/dashboard/src/app/(auth)/layout.tsx
@@ -1,7 +1,25 @@
+import { Toaster } from "@bklit/ui/components/sonner";
+import { BklitLogo } from "@bklit/ui/icons/bklit";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <NuqsAdapter>
+      <div className="flex flex-1 items-center justify-center p-8 min-h-svh max-h-svh">
+        <div className="flex flex-col gap-8 w-full max-w-md ">
+          <div className="flex items-center justify-center gap-2">
+            <BklitLogo size={64} className="dark:text-white text-black" />
+            <span className="text-4xl font-bold sr-only">Bklit</span>
+          </div>
+          {children}
+        </div>
+      </div>
+
+      <Toaster closeButton />
+    </NuqsAdapter>
+  );
 }
