2f198bec7e2e5fe7696dcf7fedae0e81a6c1dc3e
Matt <mattsince87@gmail.com>
2025-12-14 14:56:48 +0200

    doc build fixed

--- apps/docs/src/lib/mdx-components.tsx
+++ apps/docs/src/lib/mdx-components.tsx
@@ -1,17 +1,23 @@
 import { Step, Steps } from "fumadocs-ui/components/steps";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import defaultMdxComponents from "fumadocs-ui/mdx";
-import * as Icons from "lucide-react";
+import {
+  Box,
+  ChartNoAxesColumnIncreasing,
+  SquareDashedMousePointer,
+} from "lucide-react";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    ...Icons,
     Tabs,
     Tab,
     Steps,
     Step,
+    Box,
+    ChartNoAxesColumnIncreasing,
+    SquareDashedMousePointer,
     ...components,
   };
 }
