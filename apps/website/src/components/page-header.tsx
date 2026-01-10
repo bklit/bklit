afc51b000e6ff0dc400bb8ff786061419642632b
Matt <mattsince87@gmail.com>
2026-01-10 16:20:45 +0200

    scaffolding

--- apps/website/src/components/page-header.tsx
+++ apps/website/src/components/page-header.tsx
@@ -1,15 +1,40 @@
 "use client";
+
 import { Badge } from "@bklit/ui/components/badge";
 import { Button } from "@bklit/ui/components/button";
+import { useIsMobile } from "@bklit/ui/hooks/use-mobile";
+import { cn } from "@bklit/ui/lib/utils";
+import { motion } from "motion/react";
 import Link from "next/link";
+import { useState } from "react";
 import { LogoDropdown } from "./logo-dropdown";
 
 export const PageHeader = () => {
+  const isMobile = useIsMobile();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <header className="fixed z-50 flex w-full bg-linear-to-b from-background to-transparent py-5 sm:px-3 md:py-3">
-      <div className="container mx-auto max-w-7xl px-4">
-        <div className="flex items-center justify-between gap-2 rounded-xl border bg-background/60 py-1 backdrop-blur-sm sm:px-4">
-          <div className="flex items-start gap-3 pl-3 sm:pl-0">
+    <motion.header
+      animate={{ y: 0, opacity: 1 }}
+      className="fixed z-50 flex w-full bg-linear-to-b from-background to-transparent md:px-3 md:py-3"
+      initial={{ y: 30, opacity: 0 }}
+      transition={{
+        duration: 0.8,
+        ease: "easeInOut",
+        delay: 0.1,
+      }}
+    >
+      <div className="container mx-auto max-w-7xl md:px-4">
+        <motion.div
+          className="grid grid-cols-12 items-center justify-between gap-2 border-b bg-background/60 p-4 px-4 backdrop-blur-sm md:rounded-3xl md:border"
+          data-navbar
+        >
+          {/* Logo */}
+          <div className="order-1 col-span-6 flex h-9 items-start gap-3 border border-emerald-300 sm:pl-0 md:col-span-3 md:pl-3">
             <LogoDropdown />
             <Badge
               className="hidden opacity-70 hover:opacity-100 sm:block"
@@ -18,8 +43,22 @@ export const PageHeader = () => {
               Beta
             </Badge>
           </div>
-          <nav className="hidden p-4 md:block">
-            <ul className="flex items-center gap-2">
+
+          {/* Navigation */}
+          <motion.nav
+            animate={{ opacity: 1, y: 0 }}
+            className={cn(
+              "order-3 col-span-12 hidden items-center justify-center border border-emerald-300 md:order-2 md:col-span-6 md:flex md:h-9",
+              isMenuOpen && "visible flex border-rose-500 md:hidden md:flex-col"
+            )}
+            initial={{ opacity: 0, y: -7 }}
+            transition={{
+              duration: 1.2,
+              ease: "easeInOut",
+              delay: 0.1,
+            }}
+          >
+            <ul className="flex flex-col items-center gap-2 md:flex-row">
               <li>
                 <Button asChild variant="ghost">
                   <Link href="/#product">Product</Link>
@@ -52,10 +91,11 @@ export const PageHeader = () => {
                 </Button>
               </li>
             </ul>
-          </nav>
+          </motion.nav>
 
-          <nav className="flex items-center gap-2">
-            <Button asChild size="lg" variant="ghost">
+          {/* CTA */}
+          <nav className="order-2 col-span-6 flex h-9 items-center justify-end gap-2 border border-emerald-300 md:order-3 md:col-span-3">
+            <Button asChild size={isMobile ? "sm" : "default"} variant="ghost">
               <a
                 data-bklit-event="signin-button"
                 href="https://app.bklit.com/signin"
@@ -66,7 +106,7 @@ export const PageHeader = () => {
                 Sign in
               </a>
             </Button>
-            <Button asChild size="lg" variant="mono">
+            <Button asChild size={isMobile ? "sm" : "default"} variant="mono">
               <a
                 data-bklit-event="signup-button"
                 href="https://app.bklit.com/signup"
@@ -77,9 +117,37 @@ export const PageHeader = () => {
                 Sign up
               </a>
             </Button>
+
+            {/* Mobile Menu */}
+            <button
+              className="flex size-7 items-center justify-center"
+              onClick={handleMenuOpen}
+              type="button"
+            >
+              <div className="relative size-3">
+                <div
+                  className={cn(
+                    "absolute top-1/4 left-0 h-0.5 w-full bg-white transition-all duration-200",
+                    isMenuOpen && "top-0"
+                  )}
+                />
+                <div
+                  className={cn(
+                    "absolute top-1/4 left-0 h-0.5 w-full translate-y-0 bg-white transition-all duration-200",
+                    isMenuOpen && "top-1/2 translate-y-[-50%] opacity-80"
+                  )}
+                />
+                <div
+                  className={cn(
+                    "absolute bottom-1/4 left-0 h-0.5 w-full bg-white transition-all duration-300",
+                    isMenuOpen && "bottom-0 opacity-60"
+                  )}
+                />
+              </div>
+            </button>
           </nav>
-        </div>
+        </motion.div>
       </div>
-    </header>
+    </motion.header>
   );
 };
